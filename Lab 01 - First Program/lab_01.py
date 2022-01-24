b4c651bd3e65d49d35eb1236fd89f31a6b6eb76b
Bogdan Stronchyk <yorkshir2000@gmail.com>
2022-01-24 14:50:55 +0200

    Work on lab 1 in progress

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-# test
+print('Hi! My name is Bogdan.\n'
+      'I\'m here to learn how to code on Python\nby making a simple game.\n'
+      'I used to learn this language by myself, but\n'
+      'it was too boring and I coudn\'t figure out where to\n'
+      'implement these skills. Then I decided to try gamedev,\n'
+      'so here I am.')
