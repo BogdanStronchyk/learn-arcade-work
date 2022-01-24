2878ce2733bdf9cb03561063262c7052a7ded583
Bogdan Stronchyk <yorkshir2000@gmail.com>
2022-01-24 15:04:28 +0200

    Work on Lab 1 done.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,6 +1,9 @@
-print('Hi! My name is Bogdan.\n'
-      'I\'m here to learn how to code on Python\nby making a simple game.\n'
-      'I used to learn this language by myself, but\n'
-      'it was too boring and I coudn\'t figure out where to\n'
+print('Hi! My name is Bogdan.')
+print('I\'m here to learn how to code on Python')
+print('by making a simple game.')
+print('I used to learn this language by myself, but\n'
+      'it was too boring and I coudn\'t figure out where I can\n'
       'implement these skills. Then I decided to try gamedev,\n'
       'so here I am.')
+print('I\'m Ukrainian and English is not my native language,\n'
+      'so please sorry for the grammar issues I might not even notice:)')
