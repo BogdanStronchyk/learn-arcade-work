3658899a20c5e270f760dbf34489ddb09ca265fe
Bogdan Stronchyk <yorkshir2000@gmail.com>
2022-02-09 20:24:35 +0200

    Work on lab 9 in progress

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -1,9 +1,9 @@
-""" Sprite Sample Program
-
-
+"""
+Sprite Sample Program
 """
 
 import arcade
+import random
 
 # --- Constants ---
 SPRITE_SCALING_BOX = 0.5
@@ -11,9 +11,14 @@ SPRITE_SCALING_PLAYER = 0.5
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-PLAYER_MOVEMENT_SPEED = 9
 
-CAMERA_SPEED = 1
+SCREEN_WIDTH_MARGIN = SCREEN_WIDTH * 4
+SCREEN_HEIGHT_MARGIN = SCREEN_HEIGHT * 4 - 32
+
+
+PLAYER_MOVEMENT_SPEED = 2
+
+CAMERA_SPEED = 0.2
 
 # --- Variables ---
 
@@ -34,12 +39,18 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = None
         self.wall_list = None
+        self.grass_list = None
+        self.ceiling_list = None
+        self.platform_list = None
 
         # Set up the player
         self.player_sprite = None
 
         # This variable holds our simple "physics engine"
-        self.physics_engine = None
+        self.physics_wall = None
+        self.physics_ceiling = None
+        self.physics_grass = None
+        self.physics_platform = None
 
         # Track the current state of what key is pressed
         self.left_pressed = False
@@ -75,50 +86,56 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
+        self.grass_list = arcade.SpriteList()
+        self.ceiling_list = arcade.SpriteList()
+        self.platform_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
 
         # Create the player
         self.player_sprite = arcade.Sprite(
             ":resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 64
+        self.player_sprite.center_x = 128
+        self.player_sprite.center_y = 128
         self.player_list.append(self.player_sprite)
 
         # This identifies the player character
-        # and a list of sprites that the player character isn’t allowed to pass through.
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
-
-        # Manually create and position a box at 300, 200
-        wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
-        wall.center_x = 300
-        wall.center_y = 200
-        self.wall_list.append(wall)
-
-        # Manually create and position a box at 364, 200
-        wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
-        wall.center_x = 364
-        wall.center_y = 200
-        self.wall_list.append(wall)
+        # and a lists of sprites that the player character isn’t allowed to pass through.
+        self.physics_wall = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        self.physics_ceiling = arcade.PhysicsEngineSimple(self.player_sprite, self.ceiling_list)
+        self.physics_grass = arcade.PhysicsEngineSimple(self.player_sprite, self.grass_list)
+        self.physics_platform = arcade.PhysicsEngineSimple(self.player_sprite, self.platform_list)
 
         # Place boxes inside a loop
-        for x in range(173, 650, 64):
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
-            wall.center_x = x
-            wall.center_y = 350
-            self.wall_list.append(wall)
+        for y in range(32, SCREEN_HEIGHT_MARGIN, 64):
+            for x in range(32, SCREEN_WIDTH_MARGIN, 64):
+                grass = arcade.Sprite(":resources:images/tiles/grassMid.png", SPRITE_SCALING_BOX)
+                wall = arcade.Sprite(":resources:images/tiles/stoneCenter_rounded.png", SPRITE_SCALING_BOX)
+                ceiling = arcade.Sprite(":resources:images/tiles/bridgeB.png", SPRITE_SCALING_BOX)
+                platform = arcade.Sprite(":resources:images/tiles/grassHalf_mid.png", SPRITE_SCALING_BOX)
 
-        # --- Place walls with a list
-        coordinate_list = [[400, 500],
-                           [470, 500],
-                           [400, 570],
-                           [470, 570]]
+                if y == 32 and 32 < x <= SCREEN_WIDTH_MARGIN - 64:
+                    grass.center_x = x
+                    grass.center_y = y
+                    self.grass_list.append(grass)
 
-        # Loop through coordinates
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
-            wall.center_x = coordinate[0]
-            wall.center_y = coordinate[1]
-            self.wall_list.append(wall)
+                if 32 <= y < SCREEN_HEIGHT_MARGIN and x <= 32 or x >= SCREEN_WIDTH_MARGIN - 32:
+                    wall.center_x = x
+                    wall.center_y = y
+                    self.wall_list.append(wall)
+
+                if y == SCREEN_HEIGHT_MARGIN - 32 and 32 < x <= SCREEN_WIDTH_MARGIN - 64:
+                    ceiling.center_x = x
+                    ceiling.center_y = y
+                    self.ceiling_list.append(ceiling)
+
+                if y % 64 == 0 and 32 < x <= SCREEN_WIDTH_MARGIN - 64:
+                    if random.randrange(5) > 0:
+                        platform.center_x = x
+                        platform.center_y = y
+                        self.platform_list.append(wall)
+
+
+
 
     def scroll_to_player(self):
         """Scroll the window to the player."""
@@ -140,15 +157,21 @@ class MyGame(arcade.Window):
 
         # Draw the sprites
         self.wall_list.draw()
+        self.grass_list.draw()
+        self.ceiling_list.draw()
+        self.platform_list.draw()
         self.player_list.draw()
-        arcade.draw_rectangle_outline(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, arcade.color.BLACK, 1)
+        arcade.draw_rectangle_outline(SCREEN_WIDTH_MARGIN / 2, SCREEN_HEIGHT_MARGIN / 2,
+                                      SCREEN_WIDTH_MARGIN, SCREEN_HEIGHT_MARGIN, arcade.color.BLACK, 1)
 
         # Select the (unscrolled) camera for our GUI
         self.camera_for_gui.use()
         arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.WHITE, 24)
 
         arcade.draw_text(f"FPS: {self.fps:.2f}", SCREEN_WIDTH * 0.75, SCREEN_HEIGHT * 0.9, arcade.color.WHITE, 24)
+
+        arcade.draw_text(f"Coords: {self.player_sprite.center_x}, {self.player_sprite.center_y}",
+                         SCREEN_WIDTH * 0.05, SCREEN_HEIGHT * 0.9, arcade.color.WHITE, 24)
 
     def update(self, delta_time):
         # calculating fps
@@ -170,8 +193,10 @@ class MyGame(arcade.Window):
 
         # With the physics engine, we will instead update the sprites by using the physics engine’s update:
         # It is good to keep all the sprites updated AFTER the logics takes place, which is more logical
-        self.physics_engine.update()
-
+        self.physics_wall.update()
+        self.physics_ceiling.update()
+        self.physics_grass.update()
+        self.physics_platform.update()
         # Scroll the screen to the player
         self.scroll_to_player()
 
@@ -266,9 +291,9 @@ class MyGame(arcade.Window):
             if self.player_sprite.left - self.left_t == 0:
                 bump = False
 
-        elif self.player_sprite.right > SCREEN_WIDTH:
+        elif self.player_sprite.right > SCREEN_WIDTH_MARGIN:
             self.player_sprite.change_x = 0
-            self.player_sprite.right = SCREEN_WIDTH
+            self.player_sprite.right = SCREEN_WIDTH_MARGIN
             bump = True
             if self.player_sprite.right - self.right_t == 0:
                 bump = False
@@ -280,9 +305,9 @@ class MyGame(arcade.Window):
             if self.player_sprite.bottom - self.bottom_t == 0:
                 bump = False
 
-        elif self.player_sprite.top > SCREEN_HEIGHT:
+        elif self.player_sprite.top > SCREEN_HEIGHT_MARGIN:
             self.player_sprite.change_y = 0
-            self.player_sprite.top = SCREEN_HEIGHT
+            self.player_sprite.top = SCREEN_HEIGHT_MARGIN
             bump = True
             if self.player_sprite.top - self.top_t == 0:
                 bump = False
